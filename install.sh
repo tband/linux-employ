7343c2ee0d60c830bba06ef7cb9411754ec82457
Theo Band <thbnd26@gmail.com>
2025-08-10 10:01:07 +0200

    A bridge is created such that no network cable needs to be connected to laptop at startup. Efi support added. Option -c added,

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # ISO is the image locally available
-ISO=$PWD/linux_repair_r1.iso
+ISO=$PWD/linux_repair.iso
+COMMENT="Linux repair iso"
 # The interface that is connected to the PXE network. For a laptop this it the ethernet adapter
 # see also: nmcli device
 adapters=$(nmcli device|awk '/ethernet/ {print $1}')
@@ -16,24 +17,26 @@ RW_MOUNT=n
 # Parse command-line options
 prog_name=$(basename $0)
 help="
-${prog_name} [-idnwhH]
-  -i  <file>  distro.iso (default $ISO)
-  -d  <eth>   device
-  -n  <ip>    Server IP addres (default $IPADDRESS)
-  -w          Make the /srv/nfs mount writable. The iso will be unpacked instead of mounted
-  -h          This help
+${prog_name} [-icdnwhH]
+  -i  <file>   distro.iso (default $ISO)
+  -c <comment> What text to put in the iPXE menu, default $COMMENT
+  -d  <eth>    device
+  -n  <ip>     Server IP addres (default $IPADDRESS)
+  -w           Make the /srv/nfs mount writable. The iso will be unpacked instead of mounted
+  -h           This help
 
 Configure a Linux system to become a IPXE server for ISO installation on clients
 All arguments are optinal, but you want at least to specify the iso location
   
 Example:
-  sudo ./${prog_name} -d $DEVICE -i $ISO
+  sudo ./${prog_name} -d $DEVICE -i $ISO -c \"Linux Mint\"
 "
 
-while getopts i:d:whH opt
+while getopts i:c:d:whH opt
 do
   case $opt in
     i)ISO=$OPTARG;;
+    c)COMMENT=$OPTARG;;
     d)DEVICE=$OPTARG;DEVICE_OK=t;;
     n)IPADDRESS=$OPTARG;;
     w)RW_MOUNT=y;;
@@ -79,37 +82,35 @@ ufw disable
 # dhcpd config
 [ -r /etc/dhcp/dhcpd.conf.org ] || mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.org
 sed "s/__NET__/${NET}/g" etc/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
-[ -r /etc/default/isc-dhcp-server.org ] || mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.org
-# This tells only so serve bridge br0. A physical adapter will be added later. 
+#[ -r /etc/default/isc-dhcp-server.org ] || mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.org
+# This tells only to serve bridge br0. A physical adapter will be added later. 
 # This allows the dhcp server to start up
-sed "s/br0/${DEVICE}/g" etc/default/isc-dhcp-server > /etc/default/isc-dhcp-server
+#sed "s/br0/${DEVICE}/g" etc/default/isc-dhcp-server > /etc/default/isc-dhcp-server
 
 # Make bridge br0
-#nmcli con add con-name br0 type bridge ifname br0 
-#nmcli con modify br0 ipv4.addr "${IPADDRESS}/24" ipv4.method manual
+nmcli con delete br0 br0-slave if_server 2>/dev/null
+nmcli con add con-name br0 type bridge ifname br0 
+nmcli con modify br0 ipv4.addr "${IPADDRESS}/24" ipv4.method manual
 # add at least one interface to the bridge, but more are ok as well. But don't add an interface
 # that is already connected to an existing network (with dhcp server)
-# nmcli c add con-name br0-slave type bridge-slave ifname ${DEVICE} master br0
+nmcli c add con-name br0-slave type bridge-slave ifname ${DEVICE} master br0
+nmcli con up br0
 
-nmcli c delete if_server 2>/dev/null
-nmcli con add con-name if_server type ethernet ifname ${DEVICE} ipv4.method manual ipv4.address ${IPADDRESS}/24
+#nmcli con delete if_server 2>/dev/null
+#nmcli con add con-name if_server type ethernet ifname ${DEVICE} ipv4.method manual ipv4.address ${IPADDRESS}/24
+#nmcli con up if_server
 
 #httpd server config
 [ -r /var/www/html/demo_index.html ] || mv /var/www/html/index.html /var/www/html/demo_index.html
 sed "s/__IP__/${IPADDRESS}/g" var/www/html/menu  > /var/www/html/menu
+sed -i "s/__DESCRIPTION__/${COMMENT}/g" /var/www/html/menu
 
 # tftp config is ok, just add the data
 # see also https://ipxe.org/howto/chainloading
 # note that this version has NFS support added, so don't download directly from ipxe.org
-cp srv/tftp/undionly.kpxe /srv/tftp/
+cp srv/tftp/undionly.kpxe srv/tftp/ipxe.efi /srv/tftp/
 
 #nfs
-
-#Overlay filesystem creating
-# Nice idea, but an overlay filesystem cannot be exported by NFS.
-# First make an overlay filesystem such that it's easy to edit the contect of the iso. like adding preseed files
-# The actual version of the "iso" is served from /srv/nfs and can be edited
-
 # Define nfs exports:
 mkdir -p /srv/nfs
 grep -E "^/srv/nfs"      /etc/exports >/dev/null || cp etc/exports /etc/exports
@@ -129,12 +130,6 @@ if [[ -n "$ISO" ]] then
     grep -E "^/srv/nfs/mint" /etc/exports >/dev/null || echo "/srv/nfs/mint *(rw,sync,no_subtree_check)"                 >> /etc/exports
   fi
 fi
-
-#mkdir -p /mnt/{iso,nfs} # /tmp/{upper,work}
-#mount -t overlay overlay -olowerdir=/mnt/iso,upperdir=/tmp/upper,workdir=/tmp/work /srv/nfs
-#Add to fstab:
-#/mnt/Downloads/linux_repair_r1.iso   /mnt/iso      auto  x-systemd.requires=/,ro    0  0
-#overlay                              /srv/nfs   overlay  x-systemd.requires=/mnt/iso,lowerdir=/mnt/iso,upperdir=/tmp/upper,workdir=/tmp/work  0 0
 
 # Restart the services
 exportfs -rva
