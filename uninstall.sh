bd5c5faf277aa2849e6cd28f1c31f93168a6215b
Theo Band <thbnd26@gmail.com>
2025-08-17 19:20:08 +0200

    minor fixes

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,10 @@
 
 nmcli c delete br0 br0-slave if_server 2> /dev/null
 apt purge isc-dhcp-server tftpd-hpa apache2 nfs-kernel-server bridge-utils
-rm /etc/dhcp/dhcpd.conf* /etc/default/isc-dhcp-server*
+rm -f /etc/dhcp/dhcpd.conf* /etc/default/isc-dhcp-server*
 rm -rf /var/www
-rm /srv/tftp/*
 rm /etc/exports
-umount /srv/nfs/mint
+umount -l /srv/nfs/mint
 rm -rf /srv/nfs
 sed -i '/\/srv\/nfs/d' /etc/fstab
 ufw enable
