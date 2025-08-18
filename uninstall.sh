c47c8810474c7b56eb31da415965848ea95f50c3
Theo Band <thbnd26@gmail.com>
2025-08-18 23:32:14 +0200

    ssh server added and firewall setup as block incoming

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,15 @@
 
 nmcli c delete br0 br0-slave if_server 2> /dev/null
 apt purge isc-dhcp-server tftpd-hpa apache2 nfs-kernel-server bridge-utils
+apt remove openssh-server
 rm -f /etc/dhcp/dhcpd.conf* /etc/default/isc-dhcp-server*
 rm -rf /var/www
 rm /etc/exports
 umount -l /srv/nfs/mint
-rm -rf /srv/nfs
+rm -rf /srv/
 sed -i '/\/srv\/nfs/d' /etc/fstab
+apt install ufw
+ufw --force reset
 ufw enable
+ufw default deny incoming
+
