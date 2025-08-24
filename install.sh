7faae38f925c25def9f3e99012c29c22568ac826
Theo Band <thbnd26@gmail.com>
2025-08-24 23:09:06 +0200

    --check and --nonat argument added Cleaned up code

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 VERSION=1.0.1
 # ISO is the image locally available
-ISO=$PWD/linux_repair.iso
 COMMENT="Linux repair iso"
 # The interface that is connected to the PXE network. For a laptop this it the ethernet adapter
 # see also: nmcli device
@@ -13,17 +12,11 @@ IPADDRESS=192.168.5.1
 # If you want to edit the iso, like adding preseed data, change this to y
 RW_MOUNT=n
 
-function install_cubic () {
-  apt-add-repository universe
-  apt-add-repository ppa:cubic-wizard/release
-  apt update
-  apt install --no-install-recommends cubic
-}
-
 # Parse command-line options
 prog_name=$(basename $0)
+# Define help message
 help="
-${prog_name} [--iso <ISO> --comment <COMMENT> --device <DEVICE> --ip <IPADDRESS> --rw --help]
+${prog_name}
   --iso,-i      <file> distro.iso
   --rw          Make the /srv/nfs mount writable. The iso will be unpacked
                 instead of mounted
@@ -32,29 +25,38 @@ ${prog_name} [--iso <ISO> --comment <COMMENT> --device <DEVICE> --ip <IPADDRESS>
                 makes a custom iso. This allows to edit the preseed files
                 without recreating the iso
   --install_cubic
-                Install Cubic according to the instructions on the Cubic github page: (https://github.com/PJ-Singh-001/Cubic)
-  -c, --comment <text> What text to put in the iPXE menu, default $COMMENT
-  --device,-d   <eth>  device to use if there are more than one
-  --nat,        setup nat to this device. Internet from this device (Wifi adapter) is shared
+                Install Cubic according to the instructions on the Cubic 
+                github page: (https://github.com/PJ-Singh-001/Cubic)
+  -c, --comment <text>
+                What text to put in the iPXE menu, default \"$COMMENT\"
+  --device,-d <eth>
+                device to use if there are more than one
+  --nat <device>
+                setup nat to this device. Clients will have internet from this device
+                (Wifi adapter). Only set this up if you indeed have internet!
+  --nonat       Disable NAT (default). Clients will not have internet.
   --ip,-n <ip>
                 Server IP addres (default $IPADDRESS)
+  --check       Check the status of the running services (dhcpd tftpd apache nfs)
+                and exit
   --help,-h     This help
 
 Configure a Linux system to become a IPXE server for Linux installation on
-clients. All arguments are optinal, but you want at least to specify the iso
+clients. Most arguments are optinal, but you need at least to specify the iso
 location or the Cubic project directory.
   
-Example:
-  sudo ./${prog_name} -d $DEVICE -i $ISO -c \"Linux Mint\"
+Examples:
+  sudo ./${prog_name} -d $DEVICE -i linuxmint.iso -c \"Linux Mint\"
+  sudo ./${prog_name} -nat wlp2s0
+  sudo ./${prog_name} -nonat
 "
 
-#!/bin/bash
-
-# Define help message
-#help="Usage: script.sh --iso <ISO> --comment <COMMENT> --device <DEVICE> --ip <IPADDRESS> --rw --help"
-
+if [ $# == 0 ]; then
+  echo "$help"
+  exit 1
+fi
 # Parse long options
-OPTIONS=$(getopt -o i:c:d:n:wvhH --long iso:,cubic:,install_cubic,device:,nat:,ip:,rw,version,help -- "$@")
+OPTIONS=$(getopt -o i:c:d:n:wvhH --long iso:,cubic:,install_cubic,device:,nat:,nonatt,ip:,rw,version,check,help -- "$@")
 
 # Check if getopt returned an error
 if [ $? -ne 0 ]; then
@@ -62,6 +64,18 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 eval set -- "$OPTIONS"
+
+function install_cubic () {
+  apt-add-repository universe
+  apt-add-repository ppa:cubic-wizard/release
+  apt update
+  apt install --no-install-recommends cubic
+}
+
+function check_services () {
+  systemctl status isc-dhcp-server tftpd-hpa apache2|grep -C1 "Loaded"
+  systemctl status nfs-kernel-server|grep -E "●|Loaded|Active"
+}
 
 # Initialize variables
 DEVICE_OK=""
@@ -75,9 +89,11 @@ while true; do
     -c|--comment) COMMENT="$2"; shift 2;;
     -d|--device) DEVICE="$2"; DEVICE_OK="t"; shift 2;;
     --nat) NAT_DEVICE="$2"; shift 2;;
+    --nonatt) shift 1;;
     -n|--ip) IPADDRESS="$2"; shift 2;;
     -w|--rw) RW_MOUNT="y"; shift;;
     -v|--version)echo Linux-employ version $VERSION; exit 0;;
+    --check) check_services;exit 0;;
     -h|--help) echo "$help"; exit 1;;
     --) shift; break;;
     *) echo "$help"; exit 1;;
@@ -121,56 +137,45 @@ function source_iso_rw () {
     mount -o loop,ro $ISO /mnt/iso
     rsync -ai --delete /mnt/iso/ /srv/nfs/mint/
     rsync -ai --delete preseed /srv/nfs/mint/
+    umount /mnt/iso
 }
 
-if [[ ! -z $CUBIC  && -d "$CUBIC" ]] then
-  if [[ -r $ISO ]] then
-    echo -e "ERROR: Either specify an ISO or a CUBIC project folder, not both"
-    exit 1
+function check_adapters () {
+  CNT=$(echo $adapters|wc -w)
+  if [[ -z $DEVICE_OK && $CNT -gt 1 ]]
+  then
+  echo "More than one ethernet device found"
+  nmcli device
+  echo "Will be using $DEVICE"
+  echo "if that is not what you want use -d to specify the correct device"
+  read -n 1 -p "continue with $DEVICE? [n]"
+    echo
+    case $REPLY in
+      j|y);;
+      *)exit 1
+    esac
   fi
-fi
+}
 
-ISO=$(readlink -f $ISO)
+function install_packages () {
+  # update the repo first
+  apt update
+  # install packages
+  apt install -y isc-dhcp-server tftpd-hpa apache2 nfs-kernel-server bridge-utils
+  # optionally
+  apt install -y openssh-server iptables iptables-persistent net-tools vim
 
-CNT=$(echo $adapters|wc -w)
-if [[ -z $DEVICE_OK && $CNT -gt 1 ]]
-then
- echo "More than one ethernet device found"
- nmcli device
- echo "Will be using $DEVICE"
- echo "if that is not what you want use -d to specify the correct device"
- read -n 1 -p "continue with $DEVICE? [n]"
-  echo
-  case $REPLY in
-    j|y);;
-    *)exit 1
-  esac
-fi
+  # A bridge allows the dhcp server to start up
 
-# update the repo first
-apt update
-# install packages
-apt install -y isc-dhcp-server tftpd-hpa apache2 nfs-kernel-server bridge-utils
-# optionally
-apt install -y openssh-server iptables iptables-persistent net-tools vim
-
-# dhcpd config
-NET=${IPADDRESS%\.*} # 192.168.5
-[ -r /etc/dhcp/dhcpd.conf.org ] || mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.org
-sed "s/__NET__/${NET}/g" etc/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
-#[ -r /etc/default/isc-dhcp-server.org ] || mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.org
-# This tells only to serve bridge br0. A physical adapter will be added later. 
-# This allows the dhcp server to start up
-#sed "s/br0/${DEVICE}/g" etc/default/isc-dhcp-server > /etc/default/isc-dhcp-server
-
-# Make bridge br0
-nmcli con delete br0 br0-slave if_server 2>/dev/null
-nmcli con add con-name br0 type bridge ifname br0 
-nmcli con modify br0 ipv4.addr "${IPADDRESS}/24" ipv4.method manual
-# add at least one interface to the bridge, but more are ok as well. But don't add an interface
-# that is already connected to an existing network (with dhcp server)
-nmcli c add con-name br0-slave type bridge-slave ifname ${DEVICE} master br0
-nmcli con up br0
+  # Make bridge br0
+  nmcli con delete br0 br0-slave if_server 2>/dev/null
+  nmcli con add con-name br0 type bridge ifname br0 
+  nmcli con modify br0 ipv4.addr "${IPADDRESS}/24" ipv4.method manual
+  # add at least one interface to the bridge, but more are ok as well. But don't add an interface
+  # that is already connected to an existing network (with dhcp server)
+  nmcli c add con-name br0-slave type bridge-slave ifname ${DEVICE} master br0
+  nmcli con up br0
+}
 
 function enable_nat () {
   DEV_PUBLIC=$1
@@ -194,33 +199,17 @@ function enable_nat () {
   iptables -A OUTPUT -j ACCEPT
 }
 
-if [ ! -z $NAT_DEVICE ]; then
-  netfilter-persistent flush
-  enable_nat $NAT_DEVICE $DEVICE
-  netfilter-persistent save
-else
-  sed -i "s/net.ipv4.ip_forward=1/#net.ipv4.ip_forward=1/g" /etc/sysctl.conf
-  netfilter-persistent flush
+ISO=$(readlink -f $ISO)
+if [[ ! -z $CUBIC  && -d "$CUBIC" ]] then
+  if [[ -r $ISO ]] then
+    echo -e "ERROR: Either specify an ISO or a CUBIC project folder, not both"
+    exit 1
+  fi
 fi
-#nmcli con delete if_server 2>/dev/null
-#nmcli con add con-name if_server type ethernet ifname ${DEVICE} ipv4.method manual ipv4.address ${IPADDRESS}/24
-#nmcli con up if_server
-
-#httpd server config
-
-# tftp config is ok, just add the data
-# see also https://ipxe.org/howto/chainloading
-# note that this version has NFS support added, so don't download directly from ipxe.org
-cp srv/tftp/undionly.kpxe srv/tftp/ipxe.efi /srv/tftp/
-
-#nfs
-# Define nfs exports:
-mkdir -p /srv/nfs
-grep -E "^/srv/nfs"      /etc/exports >/dev/null || cp etc/exports /etc/exports
 
 function make_menu () {
   # First determine the init ramdisk file name as Cubic changes the extention depending on the compression used (lz/gz)
-  INITRD=/casper/initrd.lz
+  #INITRD=/casper/initrd.lz
   INITRD=$(awk '/initrd/ {print $2;exit}' /srv/nfs/mint/boot/grub/grub.cfg)
   [ -r /var/www/html/demo_index.html ] || mv /var/www/html/index.html /var/www/html/demo_index.html
   sed "s/__IP__/${IPADDRESS}/g" var/www/html/menu  > /var/www/html/menu
@@ -228,8 +217,22 @@ function make_menu () {
   sed -i "s%__INITRD__%${INITRD}%g" /var/www/html/menu
 }
 
+if [[ -d "$CUBIC" || -r $ISO ]] then
+  check_adapters
+  install_packages
+  # tftp config is ok, just add the data
+  # see also https://ipxe.org/howto/chainloading
+  # note that this version has NFS support added, so don't download directly from ipxe.org
+  cp srv/tftp/undionly.kpxe srv/tftp/ipxe.efi /srv/tftp/
+
+  #nfs
+  # Define nfs exports:
+  mkdir -p /srv/nfs
+  grep -E "^/srv/nfs"      /etc/exports >/dev/null || cp etc/exports /etc/exports
+fi
+
 # what kind of source?
-if [ -r $ISO ]; then
+if [ -n "$ISO" ] && [ -r "$ISO" ]; then
   if [ "$RW_MOUNT" == "y" ]; then
     source_iso_rw
   else
@@ -237,6 +240,24 @@ if [ -r $ISO ]; then
   fi
 elif [ -d "$CUBIC" ]; then
   source_cubic
+fi
+
+# dhcpd config
+NET=${IPADDRESS%\.*} # 192.168.5
+[ -r /etc/dhcp/dhcpd.conf.org ] || mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.org
+sed "s/__NET__/${NET}/g" etc/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
+
+# NAT?
+if [ ! -z $NAT_DEVICE ]; then
+  netfilter-persistent flush
+  enable_nat $NAT_DEVICE $DEVICE
+  netfilter-persistent save
+else
+  sed -i "/^\s*net.ipv4.ip_forward=1/      {s/^/#/}" /etc/sysctl.conf
+  sed -i '/^\s*option routers/             {s/^/#/}' /etc/dhcp/dhcpd.conf
+  sed -i '/^\s*option domain-name-servers/ {s/^/#/}' /etc/dhcp/dhcpd.conf
+  netfilter-persistent flush
+  netfilter-persistent save
 fi
 
 # Restart the services
