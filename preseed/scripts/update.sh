5f61cb511200a03bedee9d961992c7378da37991
Theo Band <thbnd26@gmail.com>
2025-09-21 21:18:40 +0200

    ufw stuff done first

---
+++ preseed/scripts/update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# NOTE: Script will be run during creation of ISO inside a chroot (mkiso.sh --update)
+
+PATH=$PATH:/usr/sbin
+ufw --force enable 2> /dev/null
+ufw default deny incoming
+apt-get update
+apt-get --yes upgrade
+apt-get clean
