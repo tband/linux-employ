9770fad4353c1970f5bc8880f333bf5271814f45
Theo Band <thbnd26@gmail.com>
2025-09-21 21:17:08 +0200

    renamd chroot.sh to update.sh as that is the option used for make_iso

--- preseed/scripts/chroot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# NOTE: Script will be run during creating of ISO inside a chroot (mkiso.sh --chroot)
-
-PATH=$PATH:/usr/sbin
-apt-get update
-apt-get --yes upgrade
-ufw enable
-ufw default deny incoming
